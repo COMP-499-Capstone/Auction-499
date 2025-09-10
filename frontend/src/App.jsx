73a6ad3f995bd4c8261cad59829df3dc9d039b49
Jason rincon <jason.rin1393@gmail.com>
2025-09-09 21:32:09 -0700

    initial setup-2 (misspelled something lol )

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AuthPage from "./views/authPage";
+import AuthPage from "./views/AuthPage";
 import './App.css'
 
 function App() {
